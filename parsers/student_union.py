f7ba32296ff4c02bf000c12e9afdd7ece7464472
Abhi Jain <devabhixda@gmail.com>
2025-01-14 11:36:51 +0530

    Exception handling on parser

--- parsers/student_union.py
+++ parsers/student_union.py
@@ -13,25 +13,28 @@ def parse_union(data) -> list[dict[str, Any]]:
     job_listings = []
     if matches :
         for i, match in enumerate(matches, 1):
-            cleaned_content = match.replace("\\'", "'").replace("\\n", "\n")
-            soup = BeautifulSoup(cleaned_content, 'html.parser')
-            heading = soup.find('table', class_='title').text
-            title, job_id = heading.split(' JobID: ')
-            date_posted = ""
-            for li in soup.find_all('li'):
-                label_span = li.find('span', class_='label')
-                if label_span:
-                    label = label_span.get_text(strip=True)
-                    value = li.get_text(strip=True).replace(label, "").strip()
-                    if label == 'Date Posted:':
-                        date_posted = value
-                        break
-            job_listings.append({
-                'title': title,
-                'date': date_convert_dmy(date_posted),
-                'id': job_id,
-                'link': union_base_url + job_id
-            })
+            try:
+                cleaned_content = match.replace("\\'", "'").replace("\\n", "\n")
+                soup = BeautifulSoup(cleaned_content, 'html.parser')
+                heading = soup.find('table', class_='title').text
+                title, job_id = heading.split(' JobID: ')
+                date_posted = ""
+                for li in soup.find_all('li'):
+                    label_span = li.find('span', class_='label')
+                    if label_span:
+                        label = label_span.get_text(strip=True)
+                        value = li.get_text(strip=True).replace(label, "").strip()
+                        if label == 'Date Posted:':
+                            date_posted = value
+                            break
+                job_listings.append({
+                    'title': title,
+                    'date': date_convert_dmy(date_posted),
+                    'id': job_id,
+                    'link': union_base_url + job_id
+                })
+            except Exception as e:
+                print("Unable to fetch student union jobs")
     else:
         print("No document.write content found")
     return job_listings
